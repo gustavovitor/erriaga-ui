7bdc8f71413b524dd2012846038b5ba23f546d51
Gustavo Miranda <oachgustavo@gmail.com>
2020-04-11 22:16:40 -0300

    Ajustes Heroku

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/erriaga-ui'));
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/erriaga-ui/index.html');
+});
+app.listen(process.env.PORT || 4200);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+const BASE_URL = 'https://erriaga-api.herokuapp.com/api';
+
 export const environment = {
-  production: true
+  production: true,
+
+  WhitelistedDomains: [/erriaga-api.herokuapp.com/],
+  BlacklistedDomains: [/\/api\/oauth\/token/, /\/api\/public\/user\/register/],
+
+  URL_AUTH: BASE_URL + '/oauth/token',
+  URL_USER: BASE_URL + '/public/user',
+  URL_TOKEN_REVOKE: BASE_URL + '/token/revoke',
+  URL_PERSON: BASE_URL + '/person'
 };
